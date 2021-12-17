984d3e8256c2e99620d6605498e605e081f519f3
Mcysneiros <msocys@gmail.com>
2021-12-17 16:03:25 -0300

    :tada:

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from './style';
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="title">Desenvolvimento de Website</td>
+            <td className="deposit ">R$ 21000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/06/2021</td>
+          </tr>
+          <tr>
+            <td className="title">Aluguel</td>
+            <td className="withdraw">R$ -1000,00</td>
+            <td>casa</td>
+            <td>28/06/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
